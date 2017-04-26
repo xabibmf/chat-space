d2a17de211851674a85e10d61a0d313e2f4afe3c
xabibmf <xabibmf@gmail.com>
2017-04-26 11:48:27 +0900

    Update application.rb not to generate unnecessary file when ‘rails g controller’

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,14 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
