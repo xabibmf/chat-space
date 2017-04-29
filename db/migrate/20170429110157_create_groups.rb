5cef44edbd0c7e3a8e6e3fc7bb4cc7d5f8e6d6cd
xabibmf <xabibmf@gmail.com>
2017-04-29 20:07:20 +0900

    Create group table

--- db/migrate/20170429110157_create_groups.rb
+++ db/migrate/20170429110157_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
+      t.string :name, null: false, unique: true
       t.timestamps
     end
   end
