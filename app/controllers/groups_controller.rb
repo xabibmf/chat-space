a8d1792241efa8957ca0d944c29f0a4591c48374
xabibmf <xabibmf@gmail.com>
2017-05-01 13:53:44 +0900

    Add routes of create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,4 +3,6 @@ class GroupsController < ApplicationController
   end
   def edit
   end
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :groups, only:[:new, :edit]
+  resources :groups, only:[:new, :edit, :create]
 end
