a858e91538e105b83c2aecc0bcc2f9f8f189d54b
xabibmf <xabibmf@gmail.com>
2017-05-01 21:48:03 +0900

    Add function to insert group table and group_users table

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,19 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
   end
   def edit
   end
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to( { controller: 'messages', action: 'index' }, { notice: 'グループが作成されました。' })
+    else
+      redirect_to( { controller: 'messages', action: 'index' }, { alert: 'グループ作成に失敗しました。' })
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:name, {user_ids: []})
   end
 end
