cdb3dd69f33bdcd5ca6cc99a2079229ba7b6a15e
xabibmf <xabibmf@gmail.com>
2017-05-02 12:38:53 +0900

    Show group name and group members

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,10 @@
 class MessagesController < ApplicationController
   def index
     @groups = User.find(current_user.id).groups
+    if params[:group_id]
+      @group = Group.find(params[:group_id])
+    else
+      @group = @groups.first
+    end
   end
 end
