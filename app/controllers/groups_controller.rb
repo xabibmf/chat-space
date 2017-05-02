c7a154cc09e214c17bf8455508bb45996ada6aad
xabibmf <xabibmf@gmail.com>
2017-05-02 17:50:09 +0900

    Add current_user to new group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   def new
     @group = Group.new
+    @group.user_ids << current_user.id
   end
 
   def index
@@ -11,7 +12,9 @@ class GroupsController < ApplicationController
   end
 
   def create
+    binding.pry
     @group = Group.new(group_params)
+    binding.pry
     if @group.save
       redirect_to group_messages_path(@group), notice: 'グループが作成されました。'
     else
