734322c5dd7c8212736479184f39b65e4311fb00
xabibmf <xabibmf@gmail.com>
2017-05-02 14:54:54 +0900

    Edit redirect path to prefix

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,9 +9,9 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to( { controller: 'messages', action: 'index' }, { notice: 'グループが作成されました。' })
+      redirect_to group_messages_path(@group), notice: 'グループが作成されました。'
     else
-      redirect_to( { controller: 'messages', action: 'index' }, { alert: 'グループ作成に失敗しました。' })
+      redirect_to root_path, alert: 'グループ作成に失敗しました。'
     end
   end
 
