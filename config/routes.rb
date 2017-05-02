49d9b6ee56a20cb6903fa8539800aa2cdb2cef31
xabibmf <xabibmf@gmail.com>
2017-05-02 11:51:10 +0900

    Set routes for showing private group messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :groups, only:[:new, :edit, :create]
+  resources :groups, only:[:new, :edit, :create] do
+    resources :messages, only: [:index]
+  end
 end
