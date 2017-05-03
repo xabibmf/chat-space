b845ade7c1933b9cba91bb702a2a44a865e57ce7
xabibmf <xabibmf@gmail.com>
2017-05-03 17:32:40 +0900

    Show group messages in message tables

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
+    @messages = @group.messages
     @message = Message.new
   end
 
