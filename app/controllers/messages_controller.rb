e2a2967fa63b70ba3b0926f6eca0148476354612
xabibmf <xabibmf@gmail.com>
2017-05-03 16:23:02 +0900

    Change to form_for in massage form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,10 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
+    @message = Message.new
+  end
+
+  def create
+
   end
 end
