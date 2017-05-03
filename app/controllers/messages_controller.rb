b5b339af90c15cc01362055202bfaf3c768baaed
xabibmf <xabibmf@gmail.com>
2017-05-03 18:00:13 +0900

    Redirect to message screen after message transmission

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,22 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index, :create]
   def index
     @groups = current_user.groups
-    @group = Group.find(params[:group_id])
     @messages = @group.messages
     @message = Message.new
   end
 
   def create
     @message = Message.create(body: message_params[:message][:body], group_id: message_params[:group_id], user_id: current_user.id)
+    redirect_to group_messages_path(@group)
   end
 
   private
   def message_params
     params.permit(:group_id, message: [:body])
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
