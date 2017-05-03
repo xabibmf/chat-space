e37ec15969620efe3d91563f1e54fc1ce937c38d
xabibmf <xabibmf@gmail.com>
2017-05-03 17:14:37 +0900

    Add message insert to messages table function and set strong parameter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,11 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.create(body: message_params[:message][:body], group_id: message_params[:group_id], user_id: current_user.id)
+  end
 
+  private
+  def message_params
+    params.permit(:group_id, message: [:body])
   end
 end
