746431403f8f99feab85cc2abea5b6ebbe0119af
xabibmf <xabibmf@gmail.com>
2017-05-08 19:23:24 +0900

    Add valid test for create message

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 describe Message do
-  
+  describe '#create' do
+    it "is valid" do
+      message = build(:message, body: "valid test")
+      message.valid?
+      expect(message).to be_valid
+    end
+
+    it "is invalid without a body" do
+      message = build(:message, body: "")
+      message.valid?
+      expect(message.errors[:body]).to include("translation missing: ja.activerecord.errors.models.message.attributes.body.blank")
+    end
+  end
 end
