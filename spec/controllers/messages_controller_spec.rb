44887c33b11908f3da31fdf3c8fb61d92a53cfee
xabibmf <xabibmf@gmail.com>
2017-05-09 19:27:23 +0900

    Add instances variables test for message #index

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
-
-
 describe MessagesController do
   before do
     @user = create(:user)
@@ -13,11 +11,28 @@ describe MessagesController do
   end
 
   let!(:group) { create(:group, users: [@user]) }
-  
+
   describe "GET #index" do
     it "renders the :index template" do
-      get :index, group_id: group
+      get :index, params: { group_id: group }
       expect(response).to render_template :index
+    end
+
+    it "assigns an array of messages" do
+      messages = create_list(:message, 3, user: @user, group: group)
+      get :index, params: { group_id: group }
+      expect(assigns(:messages)).to match(messages)
+    end
+
+    it "assigns an array of groups" do
+      groups = create_list(:group, 3, users: [@user]) << group
+      get :index, params: { group_id: group }
+      expect(assigns(:groups)).to match(groups.sort { |a, b| a.id <=> b.id })
+    end
+
+    it "assigns the requested group to @group" do
+      get :index, params: { group_id: group }
+      expect(assigns(:group)).to eq group
     end
   end
 end
