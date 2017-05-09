0317840d3403b417dc9c3f4f8e68e38a51edabe6
xabibmf <xabibmf@gmail.com>
2017-05-09 17:35:06 +0900

    Add message #index action view

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
+
+
 describe MessagesController do
+  before do
+    @user = create(:user)
+    sign_in @user
+  end
+
+  after do
+    sign_out @user
+  end
+
+  let!(:group) { create(:group, users: [@user]) }
   
+  describe "GET #index" do
+    it "renders the :index template" do
+      get :index, group_id: group
+      expect(response).to render_template :index
+    end
+  end
 end
