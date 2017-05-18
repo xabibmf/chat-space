05352ad8e9e5e3a6d0e0ae0e2c24f04d56c4acdb
xabibmf <xabibmf@gmail.com>
2017-05-18 20:35:59 +0900

    Show image soon after sending message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,9 +10,10 @@ $(function() {
 
   function buildHTML(message) {
     var html = $('<li class="message-list__message">')
-    html.append('<div class="message-list__message__name">' + message.user.name + '</div>');
-    html.append('<div class="message-list__message__date">' + toLocaleString(new Date(Date.parse(message.created_at))) + '</div>');
-    html.append('<div class="message-list__message__text">' + message.body + '</div>');
+    html.append(`<div class="message-list__message__name">${message.user.name}</div>
+    <div class="message-list__message__date">${toLocaleString(new Date(Date.parse(message.created_at)))}</div>
+    <div class="message-list__message__text">${message.body}</div>
+    <img src="${message.image.url}">`);
     return html;
   }
 
@@ -39,11 +40,6 @@ $(function() {
       var html = buildHTML(data.messages);
       $('.message-list').append(html);
       textField.val('');
-
-      var imagePreloader = new Image();
-      imagePreloader.onload = function() {
-        $('.message-list:last-child').children("img").attr( { 'src': data.image.url });
-      }
     })
     .fail(function() {
       alert('error');
