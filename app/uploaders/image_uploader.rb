65e4ca7ecbc6b1945eb2db69d7de9cb068fbf104
xabibmf <xabibmf@gmail.com>
2017-05-18 20:10:53 +0900

    Add function to resize upload image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
 
   storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  process resize_to_fit: [400, nil]
 end
