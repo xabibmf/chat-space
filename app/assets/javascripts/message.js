fbfa137d0ad1fbce8debb60d4265ce73fece5604
xabibmf <xabibmf@gmail.com>
2017-05-19 12:38:50 +0900

    Use template literal

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,11 +10,10 @@ $(document).on('turbolinks:load', function() {
 
   function buildHTML(message) {
     var html = $('<li class="message-list__message">')
-    var messageName = `<div class="message-list__message__name">${message.user.name}</div>`;
-    var messageDate = `<div class="message-list__message__date">${toLocaleString(new Date(Date.parse(message.created_at)))}</div>`;
-    var messageText = `<div class="message-list__message__text">${message.body}</div>`;
-    var messageImage = message.image.url ? `<img class="message-list__message__image" src="${message.image.url}">` : "";
-    html.append(`${messageName + messageDate + messageText + messageImage}`);
+    html.append(`<div class="message-list__message__name">${message.user.name}</div>
+    <div class="message-list__message__date">${toLocaleString(new Date(Date.parse(message.created_at)))}</div>
+    <div class="message-list__message__text">${message.body}</div>
+    ${message.image.url ? `<img class="message-list__message__image" src="${message.image.url}">` : ""}`);
     return html;
   }
 
