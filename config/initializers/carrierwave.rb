66688953e36d2624fd8e6282bdc7ebf7b0603cf5
xabibmf <xabibmf@gmail.com>
2017-05-19 15:18:48 +0900

    Use S3 only development and production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,21 +2,27 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'us-east-2'
-  }
+unless Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      region: 'us-east-2'
+    }
 
-  case Rails.env
-  when 'development'
-    config.fog_directory = 'chat-image'
-    config.asset_host = 'https://s3.us-east-2.amazonaws.com/chat-image'
-  when 'production'
-    config.fog_directory = 'chat-image'
-    config.asset_host = 'https://s3.us-east-2.amazonaws.com/chat-image'
+    case Rails.env
+    when 'development'
+      config.fog_directory = 'chat-image'
+      config.asset_host = 'https://s3.us-east-2.amazonaws.com/chat-image'
+    when 'production'
+      config.fog_directory = 'chat-image'
+      config.asset_host = 'https://s3.us-east-2.amazonaws.com/chat-image'
+    end
+  end
+else
+  CarrieWave.configure do |config|
+    config.storage = :file
   end
 end
