ebc539eb4a9b27b602aaa732db5798218b85d437
xabibmf <xabibmf@gmail.com>
2017-05-22 21:59:17 +0900

    Test

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  storage :fog
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
