847dab02fb4b19de9bdc5a25aa76b5c147e3267a
xabibmf <xabibmf@gmail.com>
2017-05-23 01:06:41 +0900

    Set auto_update test

--- app/assets/javascripts/auto_update.js
+++ app/assets/javascripts/auto_update.js
@@ -16,6 +16,7 @@ $(document).on('turbolinks:load', function() {
   }
 
   function update() {
+    console.log(need_to_update());
     if (!need_to_update()) {
       clearInterval(id);
       return;
