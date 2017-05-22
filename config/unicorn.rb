c9d767235b87e139f5b9fae306f38ab466aacda5
xabibmf <xabibmf@gmail.com>
2017-05-22 15:30:23 +0900

    Modify app_path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 
-working_directory app_path
+working_directory "#{app_path}/current"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
